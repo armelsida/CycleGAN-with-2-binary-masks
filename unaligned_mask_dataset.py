96736b4cad71f93eb1eb94cb73748a1ab5b7f474
Armel Sida <115725362+armelsida@users.noreply.github.com>
2024-06-21 13:37:32 +0100

    Update unaligned_mask_dataset.py

--- unaligned_mask_dataset.py
+++ unaligned_mask_dataset.py
@@ -45,11 +45,9 @@ class UnalignedMaskDataset(BaseDataset):
 
     def __getitem__(self, index):
         A_path = self.A_paths[index % self.A_size]
-        #print("A_path:", A_path)
         A_basename = os.path.basename(A_path)
         A_mask_filename = A_basename.split('.')[0] + '_binary_mask.jpg'
         A_mask_path = os.path.join(self.dir_masks_A, A_mask_filename)
-        #print("A_mask_path:", A_mask_path)
         
         if self.opt.serial_batches:
             index_B = index % self.B_size
@@ -62,54 +60,21 @@ class UnalignedMaskDataset(BaseDataset):
         B_mask_path = os.path.join(self.dir_masks_B, B_mask_filename)
         
         A_img = Image.open(A_path).convert('RGB')
-        #print("A_img dimensions:", A_img.size)
         A_mask = Image.open(A_mask_path).convert('RGB') # Load domain A mask 
-
-         # Crop or pad the mask to match the dimensions of real_A (256x256)
-        #A_mask = Image.fromarray(A_mask.astype('uint8'), mode='L')
-        #A_mask = self.crop_or_pad(A_mask, (256, 256))
-        #A_mask = np.transpose(A_mask, (1, 0))  # Transpose the mask to match image dimensions
-        #print("A_mask dimensions:", A_mask.shape)
-        #A_mask = np.tile(A_mask, (1, 1, 3))  # Expand to three channels to match the image 
-       
-        # Convert NumPy array to PIL Image
-        #A_mask_pil = Image.fromarray(A_mask.astype('uint8'))
-
         B_img = Image.open(B_path).convert('RGB')
         B_mask = Image.open(B_mask_path).convert('RGB') # Load domain B mask 
 
-        # Crop or pad the mask to match the dimensions of real_B (256x256)
-        #B_mask = Image.fromarray(B_mask.astype('uint8'), mode='L')
-        #B_mask = self.crop_or_pad(B_mask, (256, 256))
-        #B_mask = np.transpose(B_mask, (1, 0))  # Transpose the mask to match image dimensions
-        #B_mask = np.tile(B_mask, (1, 1, 3))  # Expand to three channels to match the image
-
         # Apply transformation to images and masks  
-        #A = self.transform_A(A_img).unsqueeze(0)
-        #B = self.transform_B(B_img).unsqueeze(0)
         A = self.transform_A(A_img)
         B = self.transform_B(B_img)
-        #A_mask = self.transform_A(A_mask)
-        #A_mask = torch.from_numpy(np.array(A_mask)).unsqueeze(0).float()  # Convert PIL Image back to NumPy array
-        #B_mask = self.transform_B(B_mask)
 
         # Normalize masks for domain A and B (specialized for grayscale images)
         A_mask = transforms.functional.resize(A_mask, (256, 256))
         A_mask = transforms.functional.to_tensor(A_mask).float()
-        A_mask = (A_mask - 0.5) / 0.5  # Normalize between -1 and 1
-        #A_mask = A_mask.repeat(1,3, 1, 1)  # Repeat along the channel dimension to match real_A
-
-        
+        A_mask = (A_mask - 0.5) / 0.5  # Normalize between -1 and 1  
         B_mask = transforms.functional.resize(B_mask, (256, 256))
         B_mask = transforms.functional.to_tensor(B_mask).float()
         B_mask = (B_mask - 0.5) / 0.5  # Normalize between -1 and 1
-        #B_mask = B_mask.repeat(1,3, 1, 1)  # Repeat along the channel dimension to match real_B
-        # Convert binary masks to PyTorch tensors
-        #A_mask_tensor = torch.from_numpy(A_mask).unsqueeze(0).float()
-        #A_mask_tensor = transforms.functional.to_tensor(A_mask).unsqueeze(0).float()
-        #B_mask_tensor = transforms.functional.to_tensor(B_mask).unsqueeze(0).float()
-        #A_mask_tensor = torch.from_numpy(A_mask).unsqueeze(0).float()  # Convert PIL Image back to NumPy array
-        #B_mask_tensor = torch.from_numpy(B_mask).unsqueeze(0).float()
 
         return {'A': A, 'B': B, 'A_mask': A_mask, 'B_mask': B_mask, 'A_paths': A_path, 'B_paths': B_path, 'A_mask_paths': A_mask_path, 'B_mask_paths': B_mask_path}
 
